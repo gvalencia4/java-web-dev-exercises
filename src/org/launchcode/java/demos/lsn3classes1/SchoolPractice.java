3d3639b4cf52e192db63ac033f34b3727fcdb09f
gabrielvalencia <gabev13@gmail.com>
2022-09-06 14:14:16 -0500

    4. Completed exercises

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -1,7 +1,52 @@
 package org.launchcode.java.demos.lsn3classes1;
 
+import java.util.ArrayList;
+
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student aStudent = new Student("Gabe", 1134, 1, 4.0);
+        System.out.println(aStudent.getName());
+    }
+
+    public class Teacher {
+        private String firstName;
+        private String lastName;
+        private String subject;
+        private int yearsTeaching;
+
+        // Getters
+        public String getFirstName() {
+            return firstName;
+        }
+        public String getLastName() {
+            return lastName;
+        }
+        public String getSubject() {
+            return subject;
+        }
+        public int getYearsTeaching() {
+            return yearsTeaching;
+        }
+
+        // Setters
+        private void setFirstName(String aFirstName) {
+            this.firstName = aFirstName;
+        }
+        private void setLastName(String aLastName) {
+            this.lastName = aLastName;
+        }
+        private void setSubject(String aSubject) {
+            this.subject = aSubject;
+        }
+        private void setYearsTeaching(int aYearsTeaching) {
+            this.yearsTeaching = aYearsTeaching;
+        }
+    }
+
+    public class Course {
+        private String topic;
+        private Teacher instructor;
+        private ArrayList<Student> enrolledStudents;
     }
 }
--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -3,6 +3,8 @@ package org.launchcode.java.demos.lsn3classes1;
 // Start working here with your Student class.
 // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
 
+
+
 public class Student {
 
     private String name;
@@ -10,4 +12,43 @@ public class Student {
     private int numberOfCredits = 0;
     private double gpa = 0.0;
 
+    // Constructor
+    public Student(String name, int studentId, int numberOfCredits, double gpa) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    // Getters
+    public String getName() {
+        return this.name;
+    }
+    public int getStudentId() {
+        return this.studentId;
+    }
+    public int getNumberOfCredits() {
+        return this.numberOfCredits;
+    }
+    public double getGpa() {
+        return this.gpa;
+    }
+
+    // Setters
+    private void setName(String aName) {
+        this.name = aName;
+    }
+    private void setStudentId(int aStudentId) {
+        this.studentId = aStudentId;
+    }
+    private void setGpa(double aGpa) {
+        this.gpa = aGpa;
+    }
+    private void setNumberOfCredits(int aNumberOfCredits) {
+        this.numberOfCredits = aNumberOfCredits;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }

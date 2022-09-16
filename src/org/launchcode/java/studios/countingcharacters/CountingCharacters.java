6dde9a68a72f5bb5e5aee057e3936f81fa629a74
gabrielvalencia <gabev13@gmail.com>
2022-09-15 20:58:13 -0500

    Class 2: Counting Characters studio

--- src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
+++ src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
@@ -1,19 +1,55 @@
 package org.launchcode.java.studios.countingcharacters;
 
+import java.awt.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import static java.lang.Character.toUpperCase;
+import java.util.Scanner;
 
 // Count characters in a string, either from the console or from a file.
 // Can I make this a CLI tool?
 public class CountingCharacters {
     public static void main(String[] args) throws Exception {
 
-        // Get file location
-        String filePath = "/Users/gabrielvalencia/LC101/Java/java-web-dev-exercises/src/org/launchcode/java/studios/countingcharacters/read_this_text.txt";
-        String textString = readFileAsString(filePath);
+        // Prompt user for file or typed string
+        Scanner input;
+        input = new Scanner(System.in);
+        System.out.print("Type a string or '/' to select you own text file: ");
+        String textString = input.nextLine();
+
+        if (textString.equals("")) {
+            // Get default file location
+            String filePath = "/Users/gabrielvalencia/LC101/Java/java-web-dev-exercises/src/org/launchcode/java/studios/countingcharacters/read_this_text.txt";
+            textString = readFileAsString(filePath);
+        } else if (textString.charAt(0) == '/') {
+            // Select file from prompt
+
+            // NOTE, as per: https://stackoverflow.com/questions/1241984/need-filedialog-with-a-file-type-filter-in-java
+            // "AWT isn't really the preferred way of writing Java GUI apps these days. Sun seems to have mostly abandoned it.
+            // The two most popular options are Swing and SWT. So I think they didn't really develop the APIs very extensively
+            // to add modern features. (err, to answer your question: No you don't appear to be able to do that with AWT)"
+
+            FileDialog dialog = new FileDialog((Frame)null, "Select File to Open");
+            dialog.setMode(FileDialog.LOAD);
+            dialog.setVisible(true);
+            dialog.isModal();
+
+            // Program will get caught if user clicks away from the window, setModalityType doesn't work (maybe this would work in a browser)
+            dialog.setModalityType(Dialog.ModalityType.APPLICATION_MODAL);
+
+            // Limit file selection to text files (works, but only for a second)
+            // dialog.setFilenameFilter("*.txt;");
+
+            // Return filepath and get string within .txt
+            String filePath = dialog.getDirectory() + dialog.getFile();
+            System.out.print(filePath);
+            textString = readFileAsString(filePath);
+        }
+
+        // Remove non-alpha characters (thanks Toby)
+        textString = textString.replaceAll("[^a-zA-Z]", "");
 
         // Convert a string into an array of characters
         char[] characterArray = textString.toCharArray();
